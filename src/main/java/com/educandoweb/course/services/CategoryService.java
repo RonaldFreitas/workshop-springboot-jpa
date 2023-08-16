41287dfdc71bf3a0be5027bcc1f992c5cb77d89a
Ronald Freitas <ronaldfreitasds@gmail.com>
2023-08-16 19:44:32 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/CategoryService.java
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -13,13 +13,13 @@ import com.educandoweb.course.repositories.CategoryRepository;
 public class CategoryService {
 
 	@Autowired
-	private CategoryRepository userRepository;
+	private CategoryRepository repository;
 	
 	public List<Category> findAll() {
-		return userRepository.findAll();
+		return repository.findAll();
 	}
 	public Category findById(Long id) {
-		Optional<Category> obj = userRepository.findById(id);
+		Optional<Category> obj = repository.findById(id);
 		return obj.get();
 	}
 }
